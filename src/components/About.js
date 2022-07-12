601b3336dc3ecfef73d329119d0c51cc8cc61720
haniMuh <md.hani@hotmail.com>
2022-07-13 00:40:15 +0300

    completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function About({ about}) {
+    
+    return (
+    <aside>
+        <img src={"https://via.placeholder.com/215/logo.svg"} alt="blog logo" />
+        <p>{about}</p>
+    </aside>
+    );
+}
+
+export default About;
